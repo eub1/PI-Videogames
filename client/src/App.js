a7578e33637101ddb56958f9065f657178c17a26
eub1 <ebonforte@gmail.com>
2022-10-30 16:33:11 -0300

    intall y creo estructura  en Client

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,39 @@
 import './App.css';
+import React from 'react';
+import { Route, Switch } from "react-router-dom";
+import LandingPage from './components/LandingPage'
+import Home from './components/Home';
+import Nav from './components/Nav';
+import Pagination from './components/Pagination'
+import VideogameCard from './components/VideogameCard';
+import VideogameCreate from './components/VideogameCreate';
+import VideogameDetail from './components/VideogameDetail';
+
+
 
 function App() {
   return (
     <div className="App">
+
       <h1>Henry Videogames</h1>
+
+      <Route path='/'>
+        <Nav/>
+      </Route>
+      <Route exact path='/' component={LandingPage} />
+      <Route exact path='/home' component={Home} />
+
+      <Route exact path='/videogames' component={VideogameCard} />
+
+      <Route exact path='/pagination' component={Pagination} />
+
+      <Route exact path='/create' component={VideogameCreate} />
+
+      <Route
+        path="/videogames/:id"
+        render={({ match }) => <VideogameDetail id={match.params.id} />}
+      />
+
     </div>
   );
 }
