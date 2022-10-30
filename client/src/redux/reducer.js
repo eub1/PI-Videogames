8596371a131f0dc9aefb62fe757813a3e288752b
eub1 <ebonforte@gmail.com>
2022-10-30 17:07:46 -0300

    reducer v1

---
+++ client/src/redux/reducer.js
@@ -0,0 +1,39 @@
+import {GET_VIDEOGAMES, GET_GENRES, GET_VIDEOGAME_DETAIL, CLEAN_DETAIL} from './actions';
+
+const initialState = {
+  videogames: [],
+  genres: [],
+  videogameDeetail: {}
+};
+
+function rootReducer(state = initialState, action){
+
+  switch(action.type){
+    case GET_VIDEOGAMES:
+      return {
+        ...state,
+        videogames: action.payload, // [array de videojuegos]
+      };
+    case GET_GENRES:
+      return {
+        ...state,
+        genres: action.payload,
+      };
+    case GET_VIDEOGAME_DETAIL:
+      return {
+        ...state,
+        videogameDetail: action.payload,
+      };
+    case CLEAN_DETAIL:
+      return {
+        ...state,
+        videogameDetail: {},
+      };
+    default:
+      return { ...state };
+  }
+}
+
+
+
+export default rootReducer;
