b07e8a4def851cba9d4ff600bd17f1df3d319844
eub1 <ebonforte@gmail.com>
2022-10-31 19:00:19 -0300

    creo action FILTER_BY_GENRES

--- client/src/redux/actions.js
+++ client/src/redux/actions.js
@@ -4,6 +4,7 @@ export const GET_BY_NAME_VIDEOGAMES = "GET_BY_NAME_VIDEOGAMES";
 export const GET_GENRES = "GET_GENRES";
 export const GET_VIDEOGAME_DETAIL = "GET_VIDEOGAME_DETAIL";
 export const CLEAN_DETAIL = "CLEAN_DETAIL";
+export const FILTER_BY_GENRES = "FILTER_BY_GENRES";
 
 
 
@@ -35,12 +36,21 @@ export const getGenres = () => {
     try {
       const apiData = await axios.get("http://localhost:3001/genres");
       const genres = apiData.data;
-      dispatch({ type: GET_GENRES, payload: genres });
+      dispatch( { type: GET_GENRES, payload: [ { id: 0, name: "All Genres" }, ...genres ] } );
     } catch (error) {
       console.log(error)
     }
   };
 };
+
+export const filterByGenres = (genresId)=>{
+  return (
+    {
+      type: FILTER_BY_GENRES,
+      payload: genresId
+    }
+  )
+}
 
 export const getVideogameDetail = (id) => {
   return function (dispatch) {
