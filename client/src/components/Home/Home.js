738ddaa2958c36ae9695d304ee146c70612404c8
eub1 <ebonforte@gmail.com>
2022-10-31 09:22:10 -0300

    agrego dispatch getGenres, y map de opciones

--- client/src/components/Home/Home.js
+++ client/src/components/Home/Home.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { useState, useEffect} from 'react';
 import { useDispatch, useSelector } from 'react-redux';
-import { getAllVideogames } from '../../redux/actions';
+import { getAllVideogames, getGenres } from '../../redux/actions';
 import {Link} from 'react-router-dom';
 import VideogameCard from '../VideogameCard/VideogameCard'
 
@@ -10,14 +10,21 @@ const Home = () => {
 
   const dispatch = useDispatch();
   const allVideogames = useSelector((state) => state.videogames) // mapStateToProps (trae todo lo que esta en el estado de videogames)
+  const allGenres = useSelector((state) => state.genres)
+
 
   useEffect(()=>{
-    dispatch(getAllVideogames()) //component did mount, al montar, despacha esta accion
-  },[dispatch]) // []  para que no se genere un loop infinito de llamados, si le paso algo, [algo], va a depender de que haya 'algo', para montarse
+    dispatch(getAllVideogames()); //component did mount, al montar, despacha esta accion
+    dispatch(getGenres());
+  },[]) // []  para que no se genere un loop infinito de llamados, si le paso algo, [algo], va a depender de que haya 'algo', para montarse
 
   function handleClick(e){ // para que no se bugguee, se resetea, y trae mas videojuegos
     e.preventDefault();
     dispatch(getAllVideogames());
+  }
+  function handleSelect(e){ // para que no se bugguee, se resetea, y trae mas videojuegos
+    e.preventDefault();
+    
   }
 
   return (
@@ -27,20 +34,55 @@ const Home = () => {
       <button onClick={handleClick}>Reload Videogames</button>
       <div>
         <div>
-        <select name="asc/desc" id="select1" key="select1">
+        <select name="asc/desc" id="select1" key="select_asc/desc">
           <option value="asc" key="asc">Ascendent</option>
           <option value="desc"key="desc">Descendent</option>
         </select>
         </div>
         <div>
-        <select name="videogames" id="select2" key="select2">
+        <select name="exists/created" id="select2" key="select_exists/created">
           <option value="api">Existent</option>
           <option value="db">Created</option>
         </select>
         </div>
         <div>
-        <select name="genres" id="select3" key="select3">
-          <option value="1">Action</option>
+        <select name="genres" id="select3" key="select_genres">
+          {
+            allGenres?.map(genre => {
+              return (
+                <option value={genre.id} key={genre.id}>{genre.name}</option>
+              )
+            })
+          }
+          </select>
+          </div>
+        <div>
+        {
+          allVideogames?.map( videogame => {
+            return (
+            <VideogameCard  name= {videogame.name} image= {videogame.image} released= {videogame.released} rating= {videogame.rating} genre={videogame.genre} key={Math.random()} id={videogame.id}/>
+            );
+          })
+        }
+        </div>
+      </div>
+    </>
+  )
+
+};
+
+
+export default Home;
+
+/* 
+option: siempre necesita value, para que pueda acceder y preguntar despues.
+ - dentro del select, tengo opciones(que tienen value).
+  - ese value, es asc? => hace esto, si es desc? => hace esto
+
+*/
+
+/* 
+         <option value="1">Action</option>
           <option value="2">Adventure</option>
           <option value="3">Arcade</option>
           <option value="4">Board Games</option>
@@ -59,27 +101,5 @@ const Home = () => {
           <option value="17">Simulation</option>
           <option value="18">Sports</option>
           <option value="19">Strategy</option>
-        </select>
-        </div>
-        {
-          allVideogames?.map( videogame => {
-            return (
-            <VideogameCard  name= {videogame.name} image= {videogame.image} released= {videogame.released} rating= {videogame.rating} genre={videogame.genre} key={videogame.id}/>
-            );
-          })
-        }
-      </div>
-    </>
-  )
-
-};
-
-
-export default Home;
-
-/* 
-option: siempre necesita value, para que pueda acceder y preguntar despues.
- - dentro del select, tengo opciones(que tienen value).
-  - ese value, es asc? => hace esto, si es desc? => hace esto
 
 */
