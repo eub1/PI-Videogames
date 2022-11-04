60d73bbb74d95d1e64674688eb8709bd7e8ad34f
eub1 <ebonforte@gmail.com>
2022-11-04 20:14:32 -0300

    refactorizo getByName

--- api/src/controllers/getByNameVideogames.js
+++ api/src/controllers/getByNameVideogames.js
@@ -1,21 +1,20 @@
 const axios = require('axios');
 const { Op } = require('sequelize');
-const { Videogame } = require('../db');
-const { Genre } = require('../db');
+const { Videogame, Genre } = require('../db');
+
 require('dotenv').config();
 const {
   RAWG_API_KEY
 } = process.env;
 
 
-const getByNameApiVideogames = async (name) => {
-
+const getByNameVideogames = async (name) => {
   
+
+  // ---------------- API ---------------
     const fetchedData = await axios.get(`https://api.rawg.io/api/games?search=${name}&key=${RAWG_API_KEY}`);
 
-    const matchedApiVideogames = []
-
-    matchedApiVideogames.push( fetchedData.data.results.map((videogame) =>{
+    const matchedApiVideogames = fetchedData.data.results?.map((videogame) =>{
     return {
       id: videogame.id,
       name: videogame.name,
@@ -24,15 +23,10 @@ const getByNameApiVideogames = async (name) => {
       rating: videogame.rating,
       genres: videogame.genres?.map((genero) => genero.name)
     }
-  }))
-  matchedApiVideogames.flat();
-  // console.log("matchedApiVideogames", matchedApiVideogames);
+  })
 
-  return matchedApiVideogames;
+  //  console.log("matchedApiVideogames", matchedApiVideogames);
 
-};
-
-const getByNameDbVideogames = async (name) => {
   const dbVideogames = await Videogame.findAll({
     where: {
       name: {
@@ -48,6 +42,8 @@ const getByNameDbVideogames = async (name) => {
     },
   });
 
+  
+  // ---------------- DATABASE---------------
   const fetchedDBVideogames = dbVideogames?.map(videogame => {
 
     return {
@@ -58,27 +54,14 @@ const getByNameDbVideogames = async (name) => {
       genres: videogame.dataValues?.Genres?.map((genero) => genero.name) // undefined
     }
   })
+  // console.log("fetchedDBVideogames", fetchedDBVideogames);
 
-  return  fetchedDBVideogames;
-};
+  const foundByName = fetchedDBVideogames.concat(matchedApiVideogames)
 
-const getByNameVideogames = async(name) => {
-
-  const promises = [ getByNameDbVideogames(name), getByNameApiVideogames(name)]
-  const arrayOfMatchedVideogames = await Promise.all(promises)
-  const flattenedArray =[]
-  if(arrayOfMatchedVideogames[0].length > 0){
-    return flattenedArray = arrayOfMatchedVideogames[0]?.concat(arrayOfMatchedVideogames[1][0])
-  } else {
-    if(arrayOfMatchedVideogames[1][0].length > 15){
-      const get15FirstArray = [];
-      for(let i=0; i <= 14; i++){
-        get15FirstArray.push(arrayOfMatchedVideogames[1][0][i])
-      }
-      return get15FirstArray;
-    }
-    return arrayOfMatchedVideogames[1][0]
-  };
+  // console.log("foundByName", foundByName.length, foundByName);
+  
+  return foundByName?.length > 15 ? foundByName.slice(0,15) : foundByName;
+  
 };
 
 module.exports = getByNameVideogames;
