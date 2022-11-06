002f768b024526b99f6175d145fc67b47c4b4911
eub1 <ebonforte@gmail.com>
2022-11-06 12:49:29 -0300

    Link to videogame/:id to component VideogameCard

--- client/src/components/VideogameCard/VideogameCard.js
+++ client/src/components/VideogameCard/VideogameCard.js
@@ -1,13 +1,16 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
- const VideogameCard = ({name, image, released, rating, genre}) => {
+ const VideogameCard = ({name, image, released, rating, genre, id}) => {
   return (
     <div>
+      <Link to={`/videogame/${id}`}>
       <h3>{name}</h3>
       <img src={image} alt="img not found"/>
       <h6>{rating}</h6>
       <h6>{released}</h6>
       <h6>{genre}</h6>
+      </Link>
     </div>
   )
 };
