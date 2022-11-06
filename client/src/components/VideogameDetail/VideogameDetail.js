589728d7f0f85bb8de2bf797c5b3225850dc7053
eub1 <ebonforte@gmail.com>
2022-11-06 12:48:45 -0300

    complete VideogameDetail component

--- client/src/components/VideogameDetail/VideogameDetail.js
+++ client/src/components/VideogameDetail/VideogameDetail.js
@@ -1,13 +1,31 @@
-import React from 'react';
-import {Link} from 'react-router-dom';
+import React, {useEffect} from 'react';
+import { Link, useParams } from 'react-router-dom';
+import { useDispatch, useSelector } from "react-redux";
+import { getVideogameDetail } from '../../redux/actions'
 // import Logo from '../../../../videogame.png'
 
-const VideogameDetail = () => {
+const VideogameDetail = (props) => {
+  console.log(props);
+  const {id} = useParams();
+
+  const dispatch = useDispatch();
+  
+  useEffect(() => {
+    dispatch(getVideogameDetail(id))
+  },[dispatch]);
+  
+  const idVideogame = useSelector((state) => state.videogameDetail); // toma el estado global del reducer
   return(
-    <>
-    <h1>Este es el VideogameDetail</h1>
-    <Link to='/home'>Home</Link>
-    </>
+     <div>
+          <h1>{idVideogame["name"]}</h1>
+          <img src={idVideogame["image"] && idVideogame["image"]} alt="" width="500px" heigth="700px"/>
+          <h4>{idVideogame["description"]}</h4>
+          <h3>Released date: {idVideogame["released"]}</h3>
+          <h3>Rating: {idVideogame["rating"]}</h3>
+          <h3>Platforms: {idVideogame["platforms"]?.map(p => p + " ")}</h3>
+          <h3>Genres: {idVideogame["genre"]}</h3>
+       <Link to='/home'>Home</Link>
+    </div>
   )
 }
 
