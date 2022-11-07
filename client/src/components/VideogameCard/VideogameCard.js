fc0071201705c2073f207093d81762e252070e33
eub1 <ebonforte@gmail.com>
2022-11-07 20:50:53 -0300

    css Vcard

--- client/src/components/VideogameCard/VideogameCard.js
+++ client/src/components/VideogameCard/VideogameCard.js
@@ -3,16 +3,15 @@ import {Link} from 'react-router-dom';
 import s from './videogameCard.module.css'
 
  const VideogameCard = (props) => {
-  console.log(props)
   // {name, image, released, rating, genre, id}
   return (
     <div className={s.videogameCard_main}>
       <Link to={`/videogame/${props.id}`}>
       <h3>{props.name}</h3>
       <img src={props.image} alt="img not found" width="350px" height="200px"/>
-      <h6>{props.rating}</h6>
-      <h6>{props.released}</h6>
-      <h6>{props.genre}</h6>
+      <ul className={s.videogameCard_ul}>
+      {props.genre?.map(genero => <li key={genero}>{genero}</li>)}
+      </ul>
       </Link>
     </div>
   )
