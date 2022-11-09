c513c9543a2cfa37f68452943b75300e9574697d
eub1 <ebonforte@gmail.com>
2022-11-09 10:53:07 -0300

    css style

--- client/src/components/LandingPage/LandingPage.js
+++ client/src/components/LandingPage/LandingPage.js
@@ -10,8 +10,10 @@ export default function LandingPage(){
     <Link to='/home'>
     <button id={s.button_Landing}>Begin...</button>
     </Link>
+    <div className={s.landing_message}>
     <h3>Welcome!</h3>
     <h3>This is my SPA proyect, using PostgresSQL, Node JS, Express, React-Redux and JavaScript</h3>
+    </div>
     </div>
   )
 };
