ffb5bf8db53d0229886b58a4f6c5a264d6f2037d
eub1 <ebonforte@gmail.com>
2022-11-09 11:25:09 -0300

    css style

--- client/src/components/VideogameDetail/VideogameDetail.js
+++ client/src/components/VideogameDetail/VideogameDetail.js
@@ -19,18 +19,31 @@ const VideogameDetail = (props) => {
   },[dispatch]);
 
   const idVideogame = useSelector((state) => state.videogameDetail);
-  if(idVideogame.image){
+  if(idVideogame.description){
   return (
      <div className={s.detail_Main}>
+      <div className={s.hidden}></div>
       <div className={s.detail_Container}>
           <h1>{idVideogame["name"]}</h1>
           <img src={idVideogame["image"] && idVideogame["image"]} alt="" width="400px" heigth="350px"/>
           <h4>{idVideogame["description"]}</h4>
+        <div className={s.row}>
           <h3>Released date: {idVideogame["released"]}</h3>
           <h3>Rating: {idVideogame["rating"] && idVideogame["rating"]}</h3>
-          <h3>Platforms: {idVideogame["platforms"]?.map(p => p + " ")}</h3>
-          <h3>Genres: {idVideogame["genre"]?.map(g => g + " ")}</h3>
-      </div>
+        </div>
+          <h3>Platforms: <ul>{
+          idVideogame["platforms"]?.map(p => (
+          <li>{p}</li>
+          ))
+          }</ul>
+          </h3>
+          <h3>Genres: <ul>{
+          idVideogame["genre"]?.map(g => (
+          <li>{g}</li>
+          ))
+          }</ul>
+          </h3>
+       </div>
        <Link to='/home'><button id="detail_button">Home</button></Link>
     </div>
   )
